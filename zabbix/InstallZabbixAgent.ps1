0fe7dc179545efe86c8cd93a6f33e868943bc0c6
Alex <45463096+metaphoriya@users.noreply.github.com>
2020-10-18 15:36:44 +0300

    Update InstallZabbixAgent.ps1

--- zabbix/InstallZabbixAgent.ps1
+++ zabbix/InstallZabbixAgent.ps1
@@ -1,8 +1,9 @@
 Set-Location $env:HOMEPATH
 $dir=Get-Location
 $path=$dir.Path
+$Distr = "https://www.zabbix.com/downloads/4.0.25/zabbix_agent-4.0.25-windows-amd64-openssl.msi"
 
-Invoke-WebRequest -Uri "https://www.zabbix.com/downloads/4.2.8/zabbix_agent-4.2.8-windows-amd64-openssl.msi" -OutFile "$env:HOMEPATH\SetUserFTA.zip"
+Invoke-WebRequest -Uri $Distr
 Invoke-WebRequest -Uri "https://www.zabbix.com/downloads/4.2.8/zabbix_agent-4.2.8-windows-amd64-openssl.msi" -OutFile "$env:HOMEPATH\SetUserFTA.zip"
 
 zabbix_agentd.exe --config <ваш_файл_конфигурации> --uninstall
