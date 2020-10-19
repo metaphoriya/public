742610ce646f5b64a0424ff5d1d19d6229e5f17a
Alex <45463096+metaphoriya@users.noreply.github.com>
2020-10-19 12:06:25 +0300

    Update InstallZabbixAgent.ps1

--- zabbix/InstallZabbixAgent.ps1
+++ zabbix/InstallZabbixAgent.ps1
@@ -5,3 +5,5 @@ $Istallator = 'zabbix_agent-4.0.25-windows-amd64-openssl.msi'
 $InstallatorUri = 'https://www.zabbix.com/downloads/4.0.25/zabbix_agent-4.0.25-windows-amd64-openssl.msi'
 
 Invoke-WebRequest -Uri $InstallatorUri -OutFile $Istallator
+
+msiexec /I $Istallator HOSTNAME=TEST SERVER=zabbix.vionix.xyz /qn
