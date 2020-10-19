328691d1655bd4fad42f5a528811e9044c2da392
Alex <45463096+metaphoriya@users.noreply.github.com>
2020-10-19 13:26:34 +0300

    Update InstallZabbixAgent.ps1

--- zabbix/InstallZabbixAgent.ps1
+++ zabbix/InstallZabbixAgent.ps1
@@ -7,3 +7,5 @@ $InstallatorUri = 'https://www.zabbix.com/downloads/4.0.25/zabbix_agent-4.0.25-w
 Invoke-WebRequest -Uri $InstallatorUri -OutFile $Istallator
 
 msiexec /I $Istallator HOSTNAME=TEST SERVER=zabbix.vionix.xyz SERVERACTIVE=zabbix.vionix.xyz /qn
+
+New-NetFirewallRule -Name "Zabbix" -DisplayName "Zabbix" -Enabled:True -Profile Any -Direction Inbound -Action Allow -Protocol TCP -LocalPort 10050
