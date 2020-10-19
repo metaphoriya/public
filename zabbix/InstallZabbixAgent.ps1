60f56152683ff4cf749eba011bab222b8f59714b
Alex <45463096+metaphoriya@users.noreply.github.com>
2020-10-19 11:50:51 +0300

    Update InstallZabbixAgent.ps1

--- zabbix/InstallZabbixAgent.ps1
+++ zabbix/InstallZabbixAgent.ps1
@@ -2,10 +2,10 @@ Set-Location $env:HOMEPATH
 $dir=Get-Location
 $path=$dir.Path
 $Istallator = 'zabbix_agent-4.0.25-windows-amd64-openssl.msi'
-$InstallatorUrl = 'https://www.zabbix.com/downloads/4.0.25/$Istallator'
-$Config = 'https://github.com/metaphoriya/public/blob/main/zabbix/zabbix_agentd.conf'
+$InstallatorUri = 'https://www.zabbix.com/downloads/4.0.25/$Istallator'
+$ConfigUri = 'https://github.com/metaphoriya/public/blob/main/zabbix/zabbix_agentd.conf'
 
-Invoke-WebRequest -Uri $InstallatorUrl -OutFile $Istallator
-Invoke-WebRequest -Uri $Config
+Invoke-WebRequest -Uri $InstallatorUri -OutFile $Istallator
+Invoke-WebRequest -Uri $ConfigUri
 
 zabbix_agentd.exe --config <ваш_файл_конфигурации> --uninstall
